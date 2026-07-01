f2204ea1fd187b2d2350516ade0c25cfbd5f6fbc
Tristan Languebien <tlanguebien@gmail.com>
2026-07-01 14:35:26 +0200

    add masala_blender as package

--- user_default/masala_blender/panels.py
+++ user_default/masala_blender/panels.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+
 import bpy
 
 panel_scale_y = 1.5
@@ -13,6 +16,12 @@ class MASALA_PT_mainPanel(bpy.types.Panel):
     bl_category = "Masala"
 
     def draw(self, context: bpy.types.Context):
+        # extension path is added to sys.path at draw, because adding it in the __init__ file is not allowed
+        extension_root_dir = Path(__file__).parent.parent
+        if extension_root_dir.as_posix() not in sys.path:
+            sys.path.append(extension_root_dir.as_posix())
+
+        # Draw Masala Panel
         layout = self.layout
         assert isinstance(layout, bpy.types.UILayout)
         layout.scale_y = panel_scale_y
